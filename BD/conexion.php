ae88f160160322cddd04e4850517280fa78d0018
Raul Cruz <detretaty@gmail.com>
2023-05-13 00:23:12 -0500

    Segundos cambios

---
+++ BD/conexion.php
@@ -0,0 +1,13 @@
+
+
+<?php
+    //conexion  a la base de datos 
+    $usuario    = 'root';
+    $contrasena = '';
+    $servidor   = 'localhost';
+    $basedatos  = 'asistenciaalumnos';
+    $conexion   = mysqli_connect($servidor, $usuario, $contrasena) or die ("No se a podido conectar a la base de datos");
+    $db         = mysqli_select_db($conexion, $basedatos) or die ("Lo sentimos, pues va a ser que no se ha podido conectar a la base de datos");
+?>
+
+
